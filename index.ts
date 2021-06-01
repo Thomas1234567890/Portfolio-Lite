d1ebab6f84724ae9970195147a981353ba094ec7
Thomas1234567890 <tompatcon@gmail.com>
2021-06-01 14:17:29 +0100

    6 files modified, 2 added

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 // Import stylesheets
 import "./style/_style.scss";
 import { get_height, get_width, show_size } from "./script/_test_module";
+import {makeItRain} from './script/rain'
+makeItRain();
 
-console.clear();
-console.log(show_size());
-window.addEventListener("resize", function(event) {
-  // Write TypeScript code!
-  console.clear();
-  console.log(`height:${get_height()}`);
-  console.log(`width:${get_width()}`);
-});
+// console.clear();
+// console.log(show_size());
+// window.addEventListener("resize", function(event) {
+//   // Write TypeScript code!
+//   console.clear();
+//   console.log(`height:${get_height()}`);
+//   console.log(`width:${get_width()}`);
+// });
